5c05c0e1d1e0702b6008a0f60c29fe9be2820cfe
AsianBosses <phoboi1@gmail.com>
2015-10-12 10:18:54 -0400

    Update Assign1.java

--- Assignment1_hw/src/Assign1.java
+++ Assignment1_hw/src/Assign1.java
@@ -1,75 +1,119 @@
 import javax.swing.JOptionPane;
 
+/*
+ *Part 1- USERNAME and PASSWORD authentication.
+Develop an authentication module using JAVA. Use JOptionPanes to prompt the user to enter username and password. 
+For the first JOptionPane (USER NAME), the application will evaluate if the user name is correct 
+after the user click OK. 
+If the user name is correct then the second JOptionPane (PASSWORD) will be prompted, 
+otherwise the application will prompt the user to enter the user name again. 
+
+For the password authentication, 
+if the user enter the password wrong, the program will prompt the user to enter the password again, 
+otherwise the program will return a welcome USERNAME using JOptionPane. 
+
+There is a limit for 3 trials (for both USERNAME and PASSWORD all together). 
+
+
+The account will be locked, if the attempts are over the limit and the application will return “Contact Administrator” message. 
+If the username does not exist in the system, the application will return “Invalid Username” message.
+
+ * Part 2-VALIDATE User account type
+After part 1 is complete, we want to expand the application to check for user account using switch case. 
+The application will prompt the user with drop down list to pick type of account. 
+The application will validate username and password as usual. 
+If the username and password are correct based on the user account type, then an appropriate message will be returned 
+as indicated below. 
+
+Admin: Welcome Admin! You can update and read file.
+Student: Welcome Student! You can only read file.
+Staff: Welcome Staff! You can update, read, add, delete file.
+
+If the user select the wrong account type, the application will prompt the user to select the account again 
+until the user select the right account type.
+
+Resource:
+Import javax.swing.JOptionPane;
+String[] choices = { "Admin", "Student", "Staff"};
+String input = (String) JOptionPane.showInputDialog(null, "Choose account type...",
+"Account Type",JOptionPane.QUESTION_MESSAGE, null,choices,choices[1]); 
+System.out.println(input);
+
+ */
 public class PrintNumbers {
 	public enum AccountType {
 		Admin, Staff, Student
 	};
 
 	public static void main(String[] args) {
-		String[] user = { "cqd2292", "name", "csc200" };
-		String[] password = { "password", "pass", "not" };
-		String[] acType = { "Admin", "Staff", "Student" };
-		String[] TrueAccount = { "cqd2292passwordAdmin" };
-		boolean found = false;
-
+		String[] userNames = { "admin", "staff", "student" };
+		String[] passwords = { "admin1", "staff1", "student1" };
 		AccountType[] account = { AccountType.Admin, AccountType.Staff, AccountType.Student };
 
-		String euser = JOptionPane.showInputDialog("Enter Username");
-		/*
-		 * if (x.equals(user[0])) { } int y =0; int a = 0;
-		 */
-		// if(euser.equals(user[0]))
-		String x = "", y = "", z = "", a = "";
-		x.equals(user[0]);
-		y.equals(password[0]);
-		z.equals(acType[0]);
-		a.equals(x + y + z);
+		boolean foundUsername = false;
+		boolean foundPassword = false;
+
+		String userStr = JOptionPane.showInputDialog("Enter Username");
+
+		int limit = 1;
 		int i = 0;
-		while (i <= 2) {
-			if (euser.equals(user[i])) {
-				found = true;
-				// break;
+
+		while (limit < 3) {
+			while (i < 3) {
+				if (userStr.equals(userNames[i])) {
+					foundUsername = true;
+					break;
+				}
+				i++;
 			}
 
-			// i++;
-
-			if (found) {
-				String epass = JOptionPane.showInputDialog("Enter Password");
-				if (epass.equals(password[i])) {
-					AccountType select = (AccountType) JOptionPane.showInputDialog(null, "Select account type",
-							"Account Type", JOptionPane.INFORMATION_MESSAGE, null, account, account[0]);
-					String b = "";
-					b.equals(euser + epass + select);
-					switch (select) {
-					case Admin:
-						JOptionPane.showMessageDialog(null, "Welcome Admin! You can update and read file.");
-						break;
-					case Staff:
-						JOptionPane.showMessageDialog(null, "Welcome Staff! You can update, read, add, delete file.");
-						break;
-					case Student:
-						JOptionPane.showMessageDialog(null, "Welcome Student! You can only read file.");
-						break;
-					default:
+			if (foundUsername) {
+				String passStr = JOptionPane.showInputDialog("Enter Password");
+				while (i < 3) {
+					limit = 0;
+					if (passStr.equals(passwords[i])) {
+						foundPassword = true;
 						break;
 					}
-					break;
-				} else {
-
-					JOptionPane.showMessageDialog(null, "Invalid Password");
 					i++;
 				}
+				if (foundPassword) {
+					JOptionPane.showMessageDialog(null, "Welcome " + userNames[i]);
+					AccountType input = (AccountType)JOptionPane.showInputDialog(
+							null, "Select account type", "Account Type", 
+							JOptionPane.INFORMATION_MESSAGE, null, account, account[0]);
+					switch (input) {
+						case Admin:
+							JOptionPane.showMessageDialog(null, "Welcome Admin! You can update and read file.");
+							System.exit(0);
+							// break;
+						case Staff:
+							JOptionPane.showMessageDialog(null, "Welcome Staff! You can update, read, add, delete file.");
+							System.exit(0);
+							// break;
+						case Student:
+							JOptionPane.showMessageDialog(null, "Welcome Student! You can only read file.");
+							System.exit(0);
+							// break;
+						default:
+							break;
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "Invalid Password");
+					limit++;
+				}
 			} else {
-
 				JOptionPane.showMessageDialog(null, "Invalid Username");
-				i++;
-				euser = JOptionPane.showInputDialog("Enter Username");
+				userStr = JOptionPane.showInputDialog("Enter Username");
+				limit++;
 			}
-		}
-		while (i >= 3) {
-			JOptionPane.showMessageDialog(null, "Account is locked");
-			break;
-		}
 
+		}
+		JOptionPane.showMessageDialog(null, "Account is locked. Please contact administrator.");
 	}
+
 }
+
+
+
+
